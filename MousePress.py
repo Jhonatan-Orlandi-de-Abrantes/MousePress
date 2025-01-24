0c76b9315d07c4b453e0eeab12a2dc5af0e71f73
Heddi1107 <N50natanohj2020@gmail.com>
2025-01-24 19:37:27 -0300

    Add files via upload

---
+++ MousePress.py
@@ -0,0 +1,32 @@
+from keyboard import on_press_key
+from mouse import press, release, hold
+from time import sleep
+import mouse
+
+mouse
+condicao = False
+
+
+def toggle_condicao(e):
+    global condicao
+    
+    condicao = not condicao
+    print(f'Condicao = {condicao}')
+
+def verificar_true():
+    while True:
+        if condicao:
+            hold('left')
+            while condicao == True:
+                sleep(1)
+        else:
+            if condicao == False:
+                release('left')
+                while condicao == False:
+                    sleep(1)
+                
+        
+        sleep(0.1)
+
+on_press_key('F4', toggle_condicao)
+verificar_true()
