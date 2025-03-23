b8f2dfa3e17348d585e5d1ac0e6282da2497ed4a
Jhonatan Orlandi de Abrantes <194443620+Jhonatan-Orlandi-de-Abrantes@users.noreply.github.com>
2025-03-23 15:56:43 -0300

    Add files via upload

---
+++ MousePress-1min.py
@@ -0,0 +1,41 @@
+from keyboard import on_press_key
+from mouse import click
+from time import sleep
+
+condicao = False
+min = 0
+hora = 0
+
+def calculo_tempo():
+    global min, hora
+    
+    min += 1
+    
+    if min == 60:
+        hora += 1
+        min = 0
+        
+    print(f'{hora}h {min}min')
+
+def toggle_condicao(e):
+    global condicao
+    
+    condicao = not condicao
+    print(f'Condicao = {condicao}')
+
+def verificar_true():
+    global min, hora
+    
+    while True:
+        if condicao:
+            click()
+            print('*Click*')
+            sleep(60)
+            calculo_tempo()
+        else:
+            min = 0
+            hora = 0
+            sleep(0.1)
+
+on_press_key('F4', toggle_condicao)
+verificar_true()
